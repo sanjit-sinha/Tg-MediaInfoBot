53a3f5cc42d8caca9405aa7ae22717a366bd0875
sαɴᴊɪᴛ sɪɴʜα <sanjitthegamer@gmail.com>
2023-01-08 17:13:13 +0530

    Update functions.py

--- TelegramBot/helpers/functions.py
+++ TelegramBot/helpers/functions.py
@@ -1,39 +1,9 @@
-from TelegramBot.config import *
-from pyrogram.types import Message
-from pyrogram.enums import ChatType, ChatMemberStatus
-
 import os
 import string
 import shutil
 import random
 
 
-async def isAdmin(message: Message) -> bool:
-    """
-    Return True if the message is from owner or admin of the group or sudo of the bot.
-    """
-
-    if not message.from_user:
-        return False
-    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.CHANNEL]:
-        return False
-
-    client = message._client
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-    check_status = await client.get_chat_member(chat_id, user_id)
-
-    if user_id in SUDO_USERID:
-        return True
-    elif check_status.status in [
-        ChatMemberStatus.OWNER,
-        ChatMemberStatus.ADMINISTRATOR,
-    ]:
-        return True
-    else:
-        return False
-
-    
 
 def get_readable_time(seconds: int) -> str:
     """
