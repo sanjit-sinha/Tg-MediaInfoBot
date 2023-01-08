eae00044237894ea401079f17f10da679718adb9
sαɴᴊɪᴛ sɪɴʜα <sanjitthegamer@gmail.com>
2023-01-08 17:19:17 +0530

    Update config.py

--- TelegramBot/config.py
+++ TelegramBot/config.py
@@ -10,12 +10,6 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 COMMAND_PREFIXES = prefixes = dict(prefixes=json.loads(getenv("COMMAND_PREFIXES")))
 OWNER_USERID = json.loads(getenv("OWNER_USERID"))
 
-SUDO_USERID = OWNER_USERID 
-try: SUDO_USERID += json.loads(getenv("SUDO_USERID")) 
-except: pass
-SUDO_USERID = list(set(SUDO_USERID))
-
-MONGO_URI = getenv("MONGO_URI")
 
 
 
