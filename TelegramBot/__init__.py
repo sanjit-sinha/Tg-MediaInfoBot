bb34d0990970fd59c63f841014e245b4d522c84e
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-03-01 16:59:21 +0530

    Update __init__.py

--- TelegramBot/__init__.py
+++ TelegramBot/__init__.py
@@ -1,25 +1,28 @@
-from uvloop import install
-from asyncio import get_event_loop, new_event_loop, set_event_loop
-from TelegramBot.logging import LOGGER
-from TelegramBot.config import *
-from pyrogram import Client
-import time
-import json
-import sys
 import os
+import sys
+import json
+import time
+import shutil
+from asyncio import get_event_loop, new_event_loop, set_event_loop
+import uvloop 
+import requests
 
-install()
+from TelegramBot.config import *
+from TelegramBot.logging import LOGGER
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+uvloop.install()
 LOGGER(__name__).info("Starting TelegramBot....")
 BotStartTime = time.time()
 
+
 VERSION_ASCII = """
   =============================================================
-  You MUST need to be on python 3.9 or above, shutting down the bot...
+  You MUST need to be on python 3.7 or above, shutting down the bot...
   =============================================================
   """
 
-if sys.version_info[0] < 3 or sys.version_info[1] < 9:
+if sys.version_info[0] < 3 or sys.version_info[1] < 7:
     LOGGER(__name__).critical(VERSION_ASCII)
     sys.exit(1)
 
@@ -33,8 +36,9 @@ if not os.path.exists("credentials.json"):
 
 with open(f"{os.getcwd()}/token.json", "r") as file:
     access_token = file.read()
-
 access_token = json.loads(access_token)
+
+
 
 BANNER = """
 ____________________________________________________________________
@@ -47,8 +51,9 @@ ____________________________________________________________________
 |                    __/ |                                         |
 |__________________________________________________________________|   
 """
-
+LOGGER(__name__).info(BANNER)
 LOGGER(__name__).info("setting up event loop....")
+
 
 try:
     loop = get_event_loop()
@@ -56,10 +61,20 @@ except RuntimeError:
     set_event_loop(new_event_loop())
     loop = get_event_loop()
 
-LOGGER(__name__).info(BANNER)
-LOGGER(__name__).info("initiating the client....")
 
-# https://docs.pyrogram.org/topics/smart-plugins
+async def clear_download():
+  """Clear download directory after every 24 hour."""
+  
+  shutil.rmtree(name)
+  os.mkdir(name)
+
+
+scheduler = AsyncIOScheduler()
+scheduler.add_job(clear_download, "interval", minutes=1440)
+scheduler.start()
+
+
+LOGGER(__name__).info("initiating the client....")
 plugins = dict(root="TelegramBot/plugins")
 bot = Client(
     "TelegramBot",
