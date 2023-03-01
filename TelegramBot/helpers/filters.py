13374b8abfa46a5065f0a0f564ca8821f1d66579
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-03-01 15:50:33 +0530

    Update filters.py

--- TelegramBot/helpers/filters.py
+++ TelegramBot/helpers/filters.py
@@ -1,9 +1,38 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from TelegramBot.config import OWNER_USERID
+from TelegramBot.config import SUDO_USERID
 
 
-def dev_users(_, __, message: Message) -> bool:
-    return message.from_user.id in OWNER_USERID if message.from_user else False
+def authorized(_, __, message: Message) -> bool:
+    """
+    returns True if message is from sudo user or authorized chat,
+    False otherwise. 
+    """
+     
+    if not message.from_user:
+        return False         
+    if 0 in AUTHORIZED_CHATS:
+    	return True
+    if message.chat.id in AUTHORIZED_CHATS:
+    	return True    	           
+    if message.from_user.id in SUDO_USERID:
+    	return True
 
-dev_cmd = filters.create(dev_users)
+    return False
+  
+      
+def sudo_users(_, __, message: Message) -> bool:
+    """
+    returns True if message is from sudo user, False otherwise. 
+    """
+     
+    if not message.from_user:
+        return False              
+    if message.from_user.id in SUDO_USERID:
+    	return True	
+    	
+    return False
+ 
+       
+check_auth = filters.create(authorized)
+sudo_cmd = filters.create(sudo_users)
