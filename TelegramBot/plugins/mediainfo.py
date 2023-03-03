b21adbd03f721d4e2b531e3f133796ba02f7f7d4
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-03-03 23:40:59 +0530

    Update mediainfo.py

--- TelegramBot/plugins/mediainfo.py
+++ TelegramBot/plugins/mediainfo.py
@@ -1,46 +1,49 @@
-
-from TelegramBot.helpers.supported_url_regex import SUPPORTED_URL_REGEX
-from TelegramBot.helpers.gdrivehelper import GoogleDriveHelper
-from TelegramBot.helpers.pasting_services import katbin_paste
-from TelegramBot.helpers.functions import *
-from TelegramBot.config import prefixes
-
-from pyrogram.types import Message
-from pyrogram import Client, filters
-
-from googleapiclient.http import MediaIoBaseDownload
-from googleapiclient.discovery import build
-
-from urllib.parse import unquote
-import subprocess
-import requests
-import json
 import re
 import os
+import json
+import requests
+import subprocess
+from urllib.parse import unquote
+
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from TelegramBot.helpers.functions import *
+from TelegramBot.helpers.filters import check_auth
+from TelegramBot.helpers.mediainfo_paste import mediainfo_paste
+from TelegramBot.helpers.gdrivehelper import GoogleDriveHelper
 
 
-async def gdrive_mediainfo(_, message, url):
+async def gdrive_mediainfo(message, url):
     """
-    Generates Mediainfo from a Google Drive file
+    Generates Mediainfo from a Google Drive file.
     """
 
+    reply_msg = await message.reply_text("Generating Mediainfo, Please wait..", quote=True)
     try:
         GD = GoogleDriveHelper()
         metadata = GD.get_metadata(url)
         file_id = GD.get_id(url)
+        
+        rand_str = randstr()
+        download_path = f"download/{rand_str}_{file_id}"
 
         service = build('drive', 'v3', cache_discovery=False, credentials=GD.get_credentials())
-        request = service.files().get_media(fileId=file_id)
-
-        reply_msg = await message.reply_text("Generating Mediainfo, Please wait..", quote=True)
-        with open(f"{file_id}", "wb") as file:
+        request = service.files().get_media(fileId=file_id)        
+        
+        with open(download_path, "wb") as file:
             downloader = MediaIoBaseDownload(file, request)
             downloader.next_chunk()
+            
+        mediainfo = await async_subprocess(f"mediainfo {download_path}")
+        mediainfo_json = await async_subprocess(f"mediainfo {download_path} --Output=JSON")
+        mediainfo_json = json.loads(mediainfo_json)
 
-        mediainfo = subprocess.check_output(['mediainfo', file_id]).decode("utf-8")
-        mediainfo_json = json.loads(subprocess.check_output(['mediainfo', file_id, '--Output=JSON']).decode("utf-8"))
-
-        filesize = get_readable_filesize(float(metadata['size']))
+        filesize = get_readable_bytes(float(metadata['size']))
         filename = metadata['name']
 
         lines = mediainfo.splitlines()
@@ -60,49 +63,55 @@ async def gdrive_mediainfo(_, message, url):
                 lines[i] = ''
 
         remove_N(lines)
-        with open(f"{file_id}.txt", 'w') as f:
+        with open(f"{download_path}.txt", 'w') as f:
             f.write('\n'.join(lines))
 
-        with open(f"{file_id}.txt", "r+") as file:
+        with open(f"{download_path}.txt", "r+") as file:
             content = file.read()
-        output = await katbin_paste(content)
+            
+        output = mediainfo_paste(text=content, title=filename)
+        await reply_msg.edit(f"**File Name :** `{filename}`\n\n**Mediainfo :** {output}", disable_web_page_preview=False)
+        
+        os.remove(f"{download_path}.txt")
+        os.remove(f"{download_path}")
 
-        await reply_msg.edit(f"**File Name :** `{filename}`\n\n**Mediainfo :** {output}", disable_web_page_preview=True)
-        os.remove(f"{file_id}.txt")
-        os.remove(file_id)
-
-    except:
+    except Exception as error:
         await reply_msg.delete()
         return await message.reply_text(
-            f"Something went wrong with that Gdrive url.\n\n ( make sure that the given drive url is non rate limited , public and not a folder )",
-            quote=True)
+            f"Something went wrong while processing Gdrive link.\n\n (Make sure that the gdrive url is not rate limited, is public and not a folder)", quote=True)
 
 
-async def ddl_mediainfo(_, message, url):
+    
+async def ddl_mediainfo(message, url):
     """
     Generates Mediainfo from a Direct Download Link.
     """
 
+    reply_msg = await message.reply_text("Generating Mediainfo, Please wait...", quote=True)
     try:
         filename = re.search(".+/(.+)", url).group(1)
-        reply_msg = await message.reply_text("Generating Mediainfo, Please wait..", quote=True)
-
-        with requests.get(url, stream=True, timeout=5) as r:
-            with open(filename, 'wb') as f:
+        if len(filename) > 60:
+        	filename = filename[-60:]
+        	
+        rand_str = randstr()
+        download_path = f"download/{rand_str}_{filename}"
+        	   
+        with requests.get(url, stream=True) as r:
+            with open(download_path, 'wb') as f:
                 for chunk in r.iter_content(50000000): f.write(chunk); break
 
-        mediainfo = subprocess.check_output(['mediainfo', filename]).decode("utf-8")
-        mediainfo_json = json.loads(subprocess.check_output(['mediainfo', filename, '--Output=JSON']).decode("utf-8"))
+        mediainfo = await async_subprocess(f"mediainfo {download_path}")
+        mediainfo_json = await async_subprocess(f"mediainfo {download_path} --Output=JSON")
+        mediainfo_json = json.loads(mediainfo_json)
 
         filesize = requests.head(url).headers.get('content-length')
-
         lines = mediainfo.splitlines()
         for i in range(len(lines)):
             if 'Complete name' in lines[i]:
                 lines[i] = re.sub(r": .+", ': ' + unquote(filename), lines[i])
 
             elif 'File size' in lines[i]:
-                lines[i] = re.sub(r": .+", ': ' + get_readable_filesize(float(filesize)), lines[i])
+                lines[i] = re.sub(r": .+", ': ' + get_readable_bytes(float(filesize)), lines[i])
 
             elif 'Overall bit rate' in lines[i] and 'Overall bit rate mode' not in lines[i]:
                 duration = float(mediainfo_json['media']['track'][0]['Duration'])
@@ -112,122 +121,125 @@ async def ddl_mediainfo(_, message, url):
             elif 'IsTruncated' in lines[i] or 'FileExtension_Invalid' in lines[i]:
                 lines[i] = ''
 
-        with open(f'{filename}.txt', 'w') as f:
+        with open(f'{download_path}.txt', 'w') as f:
             f.write('\n'.join(lines))
 
-        with open(f"{filename}.txt", "r+") as file:
+        with open(f"{download_path}.txt", "r+") as file:
             content = file.read()
-        output = await katbin_paste(content)
-
+            
+        output = mediainfo_paste(text=content, title=filename)
         await reply_msg.edit(f"**File Name :** `{unquote(filename)}`\n\n**Mediainfo :** {output}",
-                             disable_web_page_preview=True)
-        os.remove(f"{filename}.txt")
-        os.remove(filename)
+                             disable_web_page_preview=False)
+                             
+        os.remove(f"{download_path}.txt")
+        os.remove(f"{download_path}")
 
-    except:
+    except Exception as error:
         await reply_msg.delete()
-        return await message.reply_text(f"Something went wrong while generating Mediainfo from the given url.",
-                                        quote=True)
+        return await message.reply_text(f"Something went wrong while generating Mediainfo from the given url.", quote=True)
 
+    
 
 async def telegram_mediainfo(client, message):
     """
     Generates Mediainfo from a Telegram File.
     """
 
-    message = message.reply_to_message
-
-    if message.text:
-        return await message.reply_text("Reply to a proper media file for generating Mediainfo.**", quote=True)
-
-    elif message.media.value == 'video':
-        media = message.video
-
-    elif message.media.value == 'audio':
-        media = message.audio
-
-    elif message.media.value == 'document':
-        media = message.document
-
-    elif message.media.value == 'voice':
-        media = message.voice
-
-    else:
-        return await message.reply_text("This type of media is not supported for generating Mediainfo.**", quote=True)
-
-    filename = str(media.file_name)
-    mime = media.mime_type
-    size = media.file_size
-
     reply_msg = await message.reply_text("Generating Mediainfo, Please wait..", quote=True)
-
-    if int(size) <= 50000000:
-        await message.download(os.path.join(os.getcwd(), filename))
-
-    else:
-        async for chunk in client.stream_media(message, limit=5):
-            with open(filename, 'ab') as f:
-                f.write(chunk)
-
-    mediainfo = subprocess.check_output(['mediainfo', filename]).decode("utf-8")
-    mediainfo_json = json.loads(subprocess.check_output(['mediainfo', filename, '--Output=JSON']).decode("utf-8"))
-    readable_size = get_readable_size(size)
-
     try:
-        lines = mediainfo.splitlines()
-        for i in range(len(lines)):
-            if 'File size' in lines[i]:
-                lines[i] = re.sub(r": .+", ': ' + readable_size, lines[i])
+    	message = message.reply_to_message
+    	if message.text:
+    		return await message.reply_text("Reply to a proper media file for generating Mediainfo.**", quote=True)
+    	
+    	elif message.media.value == 'video':
+    		media = message.video
+    	
+    	elif message.media.value == 'audio':
+    		media = message.audio
+    		
+    	elif message.media.value == 'document':
+    		media = message.document
+    	
+    	elif message.media.value == 'voice':
+    		media = message.voice
+    	
+    	else: return await message.reply_text("This type of media is not supported for generating Mediainfo.**", quote=True)
+    	
+    	filename = str(media.file_name)
+    	mime = media.mime_type
+    	size = media.file_size
+    	
+    	rand_str = randstr()
+    	download_path = f"download/{rand_str}_{filename}"
+    	
+    	if int(size) <= 50000000:
+    		await message.download(os.path.join(os.getcwd(), download_path))
+    	
+    	else:
+    	    async for chunk in client.stream_media(message, limit=5):
+    	        with open(download_path, 'ab') as f:
+    	           f.write(chunk)
+    	           
+   
+    	mediainfo = await async_subprocess(f"mediainfo '{download_path}'")
+    	mediainfo_json = await async_subprocess(f"mediainfo '{download_path}' --Output=JSON")
+    	mediainfo_json = json.loads(mediainfo_json)
 
-            elif 'Overall bit rate' in lines[i] and 'Overall bit rate mode' not in lines[i]:
+    	readable_size = get_readable_bytes(size)   	
+    	lines = mediainfo.splitlines()
+    	for i in range(len(lines)):
+    	    if 'Complete name' in lines[i]:
+    	    	lines[i] = re.sub(r": .+", ': ' + unquote(filename), lines[i])
 
-                duration = float(mediainfo_json['media']['track'][0]['Duration'])
-                bitrate_kbps = (size * 8) / (duration * 1000)
-                bitrate = get_readable_bitrate(bitrate_kbps)
-
-                lines[i] = re.sub(r": .+", ': ' + bitrate, lines[i])
-
-            elif 'IsTruncated' in lines[i] or 'FileExtension_Invalid' in lines[i]:
-                lines[i] = ''
-
-        remove_N(lines)
-        with open(f'{filename}.txt', 'w') as f:
-            f.write('\n'.join(lines))
-
-        with open(f"{filename}.txt", "r+") as file:
-            content = file.read()
-
-        output = await katbin_paste(content)
-
-        await reply_msg.edit(f"**File Name :** `{filename}`\n\n**Mediainfo :** {output}", disable_web_page_preview=True)
-        os.remove(f'{filename}.txt')
-        os.remove(filename)
-
-    except:
+    	    if 'File size' in lines[i]:
+    	    	lines[i] = re.sub(r": .+", ': ' + readable_size, lines[i])
+    	    	
+    	    elif 'Overall bit rate' in lines[i] and 'Overall bit rate mode' not in lines[i]:
+    	        duration = float(mediainfo_json['media']['track'][0]['Duration'])
+    	        bitrate_kbps = (size * 8) / (duration * 1000)
+    	        bitrate = get_readable_bitrate(bitrate_kbps)
+    	        lines[i] = re.sub(r": .+", ': ' + bitrate, lines[i])
+    	    
+    	    elif 'IsTruncated' in lines[i] or 'FileExtension_Invalid' in lines[i]:
+    	    	lines[i] = ''
+    	    
+    	remove_N(lines)
+    	with open(f'{download_path}.txt', 'w') as f:
+    		f.write('\n'.join(lines))
+    	
+    	with open(f"{download_path}.txt", "r+") as file:
+    	    content = file.read()
+    	
+    	output = mediainfo_paste(text=content, title=filename)
+    	await reply_msg.edit(f"**File Name :** `{filename}`\n\n**Mediainfo :** {output}", disable_web_page_preview=False)
+    	
+    	os.remove(f'{download_path}.txt')
+    	os.remove(download_path)
+        
+    except Exception as error:
         await reply_msg.delete()
-        await message.reply_text(f"Something went wrong while generating Mediainfo of replied Telegram file.", quote=True)
+        await message.reply_text(f"Something went wrong while generating Mediainfo from replied Telegram file.", quote=True)
 
 
 
 
-@Client.on_message(filters.command( ["mediainfo", "m"], **prefixes))
+@Client.on_message(filters.command(["mediainfo", "m"]) & check_auth)
 async def mediainfo(client, message: Message):
-    mediainfo_usage = f"**Generate mediainfo from Google Drive Links, Telegram files or direct download links. Reply to any telegram file or just pass the link after the command."
+    mediainfo_usage = f"**Generates mediainfo from Google Drive Links, Telegram files or direct download links. \n\nReply to any telegram file or just pass the link after the command."
     
     if message.reply_to_message:
         return await telegram_mediainfo(client, message)
 
-    elif len(message.command) < 2:
+    if len(message.command) < 2:
         return await message.reply_text(mediainfo_usage, quote=True)
-
-    user_url = message.text.split(None, 1)[1].split(" ")[0]
-    for (key, value) in SUPPORTED_URL_REGEX.items():
-        if bool(re.search(FR"{key}", user_url)):
-
-            if value == "gdrive":
-                return await gdrive_mediainfo(client, message, url=user_url)
-
-            elif value == "ddl":
-                return await ddl_mediainfo(client, message, url=user_url)
-
-    return await message.reply_text("This type of URL is not supported.", quote=True)
+        
+    user_input = message.text.split(None, 1)[1]    
+    if url_match := re.search(r"https://drive\.google\.com/\S+", user_input):
+    	url = url_match.group(0)
+    	return await gdrive_mediainfo( message, url)      	
+    	
+    if url_match := re.search(r"https?://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])", user_input):
+    	url = url_match.group(0)
+    	return await ddl_mediainfo(message, url)
+    	    
+    else: return await message.reply_text("This type of link is not supported.", quote=True)        
