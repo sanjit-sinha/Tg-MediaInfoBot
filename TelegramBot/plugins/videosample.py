df3b50bf63ac9c860bd68de07ed21d525a4ae871
Ashish Sharma <99271761+AshishSharma2006@users.noreply.github.com>
2023-06-18 00:24:31 +0530

    Added Ability To Generate Sample With Multiple Streams (#23)
    Generate sample video with mutliple audios and subtitiles that are available in your source video

--- TelegramBot/plugins/videosample.py
+++ TelegramBot/plugins/videosample.py
@@ -198,7 +198,7 @@ async def telegram_videosample(message, client, duration):
 
         # fixing the file metadata.
         output_path = f"download/{rand_str}_output_{filename}"
-        ffmpeg_command = f"ffmpeg -i '{download_path}' -c copy -metadata duration=10000 '{output_path}'"
+        ffmpeg_command = f"ffmpeg -ss 00:00:0 -i '{download_path}' -t 00:0'{int(duration)}':00 -map 0:v? -map 0:a? -map 0:s? -map 0:d? -c copy '{output_path}'"
         await async_subprocess(ffmpeg_command)
 
         await replymsg.edit("Uploading the video file, Please wait...")
